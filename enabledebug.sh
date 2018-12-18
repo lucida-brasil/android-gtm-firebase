8462e3120234cfbebea3e7d2059edc67ef641539
Ciro Souza <ciro.mies@gmail.com>
2018-12-17 22:23:53 -0200

    enable verbose debug

--- enabledebug.sh
+++ enabledebug.sh
@@ -1,25 +1,36 @@
 #!/bin/bash
 
-cmd="shell setprop debug.firebase.analytics.app br.com.zoly.firebase"
-run="run: adb $cmd"
+cmd1="shell setprop debug.firebase.analytics.app br.com.zoly.firebase"
+cmd2="shell setprop log.tag.FA VERBOSE"
+run="run:\nadb $cmd1\nadb $cmd2"
 
 which locate 2>\&1 > /dev/null
 if [[ $? -ne 0 ]]; then
-    echo $run;
+    echo -e $run;
     exit 1;
 fi
 
 adb=`locate adb|grep platform-tools|grep adb\$|head -n1`
 if [[ $adb == "" || ! -x $adb ]]; then
-    echo $run;
+    echo -e $run;
     exit 2;
 fi
 
-run="$adb $cmd"
+run="$adb $cmd1";
 echo "Enabling debug:";
 echo -n "$run";
 
-$run 2> /dev/null
+$run 2> /dev/null;
+if [[ $? -ne 0 ]]; then
+    echo " - Error";
+    exit 3;
+fi
+echo " - OK";
+
+run="$adb $cmd2";
+echo -n "$run";
+
+$run 2> /dev/null;
 if [[ $? -ne 0 ]]; then
     echo " - Error";
     exit 3;
